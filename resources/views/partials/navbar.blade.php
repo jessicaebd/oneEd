f9a9f731be13f48e125ac5206dd1ef7512bf5993
jessicaebd <jessica.natalia23oct@gmail.com>
2021-11-12 19:18:45 +0700

    benerin dikit

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,15 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top shadow-sm p-2 bg-body bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand me-5" href="index.html">
+
+    <a class="navbar-brand me-5" href="/">
       <img src="images/logo.png" alt="Logo" width="120" height="45" class="d-inline-block align-text-top">
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
-      aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon" style="color:blue"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
         <li class="nav-item">
           <a class="nav-link me-3" href="/courses"><b>Courses</b></a>
         </li>
@@ -23,7 +24,9 @@
           <a class="nav-link me-3" href="/scholarships"><b>Scholarships</b></a>
         </li>
       </ul>
+      
       <a href="/login" class="btn btn-primary btn-custom me-3 btn-md">Register / Login</a>
     </div>
+
   </div>
 </nav>
