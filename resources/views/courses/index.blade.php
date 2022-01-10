8572c00a693f5b7875cc857479d7ed5b4b188900
jessicaebd <jessica.natalia23oct@gmail.com>
2022-01-10 20:48:52 +0700

    ---

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -81,7 +81,7 @@
     <!-- End of featured Courses -->
 
     <section class="pt-5 pb-5">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-6">
                     <h3 class="mb-3">Carousel cards title </h3>
@@ -101,44 +101,23 @@
                         <div class="carousel-inner">
                             <div class="carousel-item active">
                                 <div class="row">
+                                    @foreach ($coursesList as $course)
+                                        <div class="col-lg-3 col-md-4 col-sm-6 mb-3 text-blue">
+                                            <div class="card" style="min-height: 150px;">
+                                                <img src="https://source.unsplash.com/1600x900?{{ $course['category'] }}"
+                                                    class="card-img-top" alt="...">
 
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <img class="img-fluid" alt="100%x280"
-                                                src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d">
-                                            <div class="card-body">
-                                                <h4 class="card-title">Special title treatment</h4>
-                                                <p class="card-text">With supporting text below as a natural lead-in to
-                                                    additional content.</p>
+                                                <div class="card-body d-flex flex-column">
+                                                    <a href="courses/{{ $course['slug'] }}" class="mt-auto">
+                                                        <h5 class="card-title"><b>{{ $course['title'] }}</b></h5>
+                                                    </a>
 
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <img class="img-fluid" alt="100%x280"
-                                                src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698">
-                                            <div class="card-body">
-                                                <h4 class="card-title">Special title treatment</h4>
-                                                <p class="card-text">With supporting text below as a natural lead-in to
-                                                    additional content.</p>
-
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-4 mb-3">
-                                        <div class="card">
-                                            <img class="img-fluid" alt="100%x280"
-                                                src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a">
-                                            <div class="card-body">
-                                                <h4 class="card-title">Special title treatment</h4>
-                                                <p class="card-text">With supporting text below as a natural lead-in to
-                                                    additional content.</p>
+                                    @endforeach
 
-                                            </div>
-                                        </div>
-                                    </div>
+
 
                                 </div>
                             </div>
