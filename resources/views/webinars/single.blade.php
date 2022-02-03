2cf4d9b5f7914b3d7c119d711344edef0979050f
jessicaebd <jessica.natalia23oct@gmail.com>
2022-02-03 19:24:50 +0700

    .

--- resources/views/webinars/single.blade.php
+++ resources/views/webinars/single.blade.php
@@ -10,10 +10,23 @@
                 </div>
 
                 <div class="col-lg-6">
+                    <div class="badge rounded-pill bg-light xs-font px-3 py-1 mb-3 text-blue">
+                        Webinar
+                    </div>
+
+                    <div class="badge rounded-pill bg-secondary xs-font px-3 py-1 mb-3 ms-2 text-light fw-normal">
+                        {{ $webinar['category'] }}
+                    </div>
 
                     <h1 class="display-5 fs-4 fw-bold lh-1 mb-3">{{ $webinar['title'] }}</h1>
+
+                    <div class="bg-light text-dark xs-font">
+                        <i class="bi bi-calendar2-day m-font"></i> {{ $webinar['eventDate'] }}
+                    </div>
+
                     <p class="lead s-font">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet reprehenderit
                         aut quia cupiditate excepturi asperiores?</p>
+
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                         <a href="#" class="btn btn-main btn-custom mt-auto" style="padding: 10px 20px">Register</a>
                     </div>
