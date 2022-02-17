6832c09aefce987103d69c740ff64b62555cc808
fredericsetievi <fredericsetievi@gmail.com>
2022-02-18 01:02:49 +0700

    scholarshipImage

--- app/Models/Scholarship.php
+++ app/Models/Scholarship.php
@@ -9,64 +9,64 @@ class Scholarship
 {
     private static $scholarship_list = [
         [
-            'slug' => 'whats-the-culture-like-to-work-in-educational-industry',
-            'title' => 'INDUSTRIAL SHARING: "What\'s the Culture like to Work in Educational Industry"',
+            'slug' => 'sea-undergraduate-scholarship',
+            'title' => 'SEA Undergratudate Scholarship Program',
             'speaker' => 'Nadila Setiabudiarto',
             'category' => 'Self Development',
             'registBy' => '10 Jan 2022',
             'eventDate' => 'Saturday, 15 Jan 2022',
             'time' => '13:00-15:00 WIB',
-            'poster' => 'webinar-1.jpeg'
+            'poster' => 'scholarship-1.jpeg'
         ],
         [
-            'slug' => 'entrepreneurship-in-time-of-crisis',
-            'title' => 'STUDIUM GENERALE: "Entrepreneurship in Time of Crisis"',
+            'slug' => 'digital-talent-scholarship',
+            'title' => 'Digital Talent Scholarship Program',
             'speaker' => 'Willson Cuaca',
             'category' => 'Self Development',
             'registBy' => '11 Jan 2022',
             'eventDate' => 'Sunday, 16 Jan 2022',
             'time' => '13:00-15:00 WIB',
-            'poster' => 'webinar-2.jpg'
+            'poster' => 'scholarship-2.jpg'
         ],
         [
-            'slug' => 'successful-entrepreneurship-in-the-new-normal-era',
-            'title' => 'STUDIUM GENERALE X WEBINAR HEBAT: "Successful Entrepreneurship in the New Normal Era"',
+            'slug' => 'bsi-scholarship-prestasi',
+            'title' => 'BSI Scholarship Prestasi Program',
             'speaker' => 'Kaesang Pangarep',
             'category' => 'Self Development',
             'registBy' => '12 Jan 2022',
             'eventDate' => 'Monday, 17 Jan 2022',
             'time' => '13:30-15:00 WIB',
-            'poster' => 'webinar-3.jpg'
+            'poster' => 'scholarship-3.jpg'
         ],
         [
-            'slug' => 'technoscape-2020-bncc',
-            'title' => 'TECHNOSCAPE 2020 – BINUS COMPUTER CLUB',
+            'slug' => 'bakti-bca-scholarship',
+            'title' => 'Bakti BCA Scholarship Program',
             'speaker' => 'TechnoScape x tiket.com',
             'category' => 'Self Development',
             'registBy' => '13 Jan 2022',
             'eventDate' => 'Tuesday, 18 Jan 2022',
             'time' => '13:30-15:00 WIB',
-            'poster' => 'webinar-4.png'
+            'poster' => 'scholarship-4.jpeg'
         ],
         [
-            'slug' => 'ideathonesia-2020-theory-of-change',
-            'title' => 'Webinar Ideathonesia 2020 : Theory of Change',
+            'slug' => 'tanoto-sahabat-pascasarjana-scholarship',
+            'title' => 'Tanoto Sahabat Pascasarjana Scholarship Program',
             'speaker' => 'Dr. Ari Margiono (CIDER Binus)',
             'category' => 'Self Development',
             'registBy' => '14 Jan 2022',
             'eventDate' => 'Tuesday, 18 Jan 2022',
             'time' => '10:25-11:30 WIB',
-            'poster' => 'webinar-5.jpeg'
+            'poster' => 'scholarship-5.jpeg'
         ],
         [
-            'slug' => 'how-millennials-plan-their-financial-for-future-serenity',
-            'title' => 'WEBINAR : "How Millennials Plan Their Financial for Future Serenity"',
+            'slug' => 'djarum-plus-scholarship',
+            'title' => 'Djarum Plus Scholarship Program',
             'speaker' => 'Dr. Ari Margiono (CIDER Binus)',
             'category' => 'Self Development',
             'registBy' => '14 Jan 2022',
             'eventDate' => 'Tuesday, 18 Jan 2022',
             'time' => '10:25-11:30 WIB',
-            'poster' => 'webinar-6.jpg'
+            'poster' => 'scholarship-6.jpeg'
         ],
     ];
 
